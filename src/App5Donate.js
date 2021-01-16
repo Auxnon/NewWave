507f211ef4f24bdf85c166028ef5c8f406cf5b82
McAvoy <nicholas.mcavoy@randstadusa.com>
2021-01-15 21:01:32 -0500

    finally adding donation options i guess

---
+++ src/App5Donate.js
@@ -0,0 +1,20 @@
+//import * as THREE from "./lib/three.module.js";
+//import * as Render from "./Render.js";
+
+//pass in name, and a pointer to a complete function which dictates everything has loaded, 
+//we keep track inside the mini class by counting  resources and incrementing till count is complete then, complte()
+//animate is called every render, deint... not used yet
+
+function init(index,dom,complete) {
+
+}
+
+function animate(delta){
+
+}
+
+function deinit(){
+
+}
+
+export {init,animate,deinit}
